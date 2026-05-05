0f7991f84d6f6db8e8fd8e67126086f26b0d5dc2
Yahui Xu <xuyahui12@gmail.com>
2026-05-04 21:16:45 -0700

    Update my photo

--- src/About.tsx
+++ src/About.tsx
@@ -75,7 +75,7 @@ const About = () => {
           {/* 渐变圆容器 */}
           <div className="relative w-50 h-50 flex items-end justify-center">
             {/* 渐变背景圆 */}
-            <div className="absolute inset-0 rounded-full shadow-2xl bg-linear-to-t from-gray-00 to-gray-200" />
+            <div className="absolute inset-0 rounded-full shadow-2xl border-t border-gray-300 bg-linear-to-t from-gray-00 to-gray-300/50 dark:to-blue-600/50 dark:border-t dark:border-blue-600" />
             {/* 矢量图，底部超出圆 */}
             <img
               src={myphoto}
